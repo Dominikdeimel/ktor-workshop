017b18f483b0332114fdf594faff48d7c1fd10bb
Dominik Deimel <domi.deimel@yahoo.de>
2020-06-12 16:10:28 +0200

    minor fix and frontend image added

--- frontend-workshop/src/components/Welcome.vue
+++ frontend-workshop/src/components/Welcome.vue
@@ -4,11 +4,24 @@
             <v-layout justify-center>
                 <v-card width="1200" style="margin-top: 30px">
                     <div style="margin-top: 10px; margin-left: 10px; margin-right: 10px">
-                    <p style="font-family:verdana; font-size: 25px">Herzlich Willkommen zum Workshop Serverseitige Entwicklung mit Ktor, im Rahmen des Moduls Web Technologien, im Medieninformatik Master an der Technischen Hochschule Köln</p>
-                    <p style="font-family:verdana; font-size: 17px">Über das Menü kann auf die Aufgaben 1 - 3 zugegriffen werden. Aufgabe 4 ist optional und wird während des Workshops angekündigt. Über den Reiter Home oder durch das aktuallisieren der seite können Sie jederzeit wieder auf diese Seite zurückkommen</p>
-                    <p style="font-family:verdana; font-size: 17px">Sollten Problemen oder Fragen, stehe ich jederzeit zur Verfügung</p>
-                        <p v-if="backendOnline === true" style="font-family:verdana; font-size: 17px; color: green"> Backend: Online</p>
-                        <p v-if="backendOnline === false" style="font-family:verdana; font-size: 17px; color: red"> Backend: Offline!</p>
+                        <p style="font-family:verdana; font-size: 20px; margin-left: 30%; width: 500px">Herzlich
+                            Willkommen zum Workshop</p>
+                        <div style="margin-bottom: 5px;">
+                            <b style="font-family:verdana; font-size: 25px; width: 500px">Serverseitige Entwicklung mit
+                                Ktor</b>
+                        </div>
+                        <p style="font-family:verdana; font-size: 20px; margin-left: 30%; width: 500px">im Rahmen des
+                            Moduls Web Technologien, im Medieninformatik Master an der Technischen Hochschule Köln</p>
+                        <p style="font-family:verdana; font-size: 17px">Über das Menü kann auf die Aufgaben 1 - 3
+                            zugegriffen werden. Aufgabe 4 ist optional und wird während des Workshops angekündigt. Über
+                            den Reiter Home oder durch das aktuallisieren der seite können Sie jederzeit wieder auf
+                            diese Seite zurückkommen</p>
+                        <p style="font-family:verdana; font-size: 17px">Sollten Problemen oder Fragenaufkommen, stehe ich
+                            jederzeit zur Verfügung</p>
+                        <p v-if="backendOnline === true" style="font-family:verdana; font-size: 17px; color: green">
+                            Backend: Online</p>
+                        <p v-if="backendOnline === false" style="font-family:verdana; font-size: 17px; color: red">
+                            Backend: Offline!</p>
                     </div>
                 </v-card>
             </v-layout>
@@ -34,7 +47,7 @@
                 axios.get(
                     'http://localhost:3000/checkStatus/'
                 ).then(res => {
-                    if(res.status === 200){
+                    if (res.status === 200) {
                         this.backendOnline = true;
                     }
                 });
